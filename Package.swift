99078977a06ece693d0903bf77df9556f2c9e7ec
Ayush Pogra <188023972+pograayush1@users.noreply.github.com>
2025-05-19 14:44:17 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CountryPicker-SwiftUI",
     platforms: [
-        .iOS(.v16),
+        .iOS(.v15),
     ],
     products: [
         .library(
